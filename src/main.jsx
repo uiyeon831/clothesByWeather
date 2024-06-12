ec3eca18fad41be466673510b158da183f8d708d
uiyeon831 <104365642+uiyeon831@users.noreply.github.com>
2024-06-12 21:08:01 +0900

    redux 설정
    redux 설정

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { router } from './App.jsx'
 import './index.css';
 import { RouterProvider } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/index';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 )
